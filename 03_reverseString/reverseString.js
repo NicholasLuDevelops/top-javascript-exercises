8bfad5f08b7c27db295e3d3031f0a273e79c1136
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-09 13:06:03 -0500

    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringReversed = Array.from(string);
 
+  let stringStart = 0;
+  let stringEnd = stringReversed.length - 1;
+
+  while (stringStart < stringEnd) {
+    [stringReversed[stringStart], stringReversed[stringEnd]] = [
+      stringReversed[stringEnd],
+      stringReversed[stringStart],
+    ];
+    stringStart++;
+    stringEnd--;
+  }
+  return stringReversed.join('');
 };
 
 // Do not edit below this line
