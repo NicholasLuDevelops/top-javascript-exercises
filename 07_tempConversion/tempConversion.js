988171b98b200d20a5edcd212a34f5fb04bcb66c
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-12 15:44:20 -0500

    all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let convertedTemp = (temperature - 32) * (5 / 9);
+  convertedTemp = Number(convertedTemp.toFixed(1));
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let convertedTemp = temperature * (9 / 5) + 32;
+  convertedTemp = Number(convertedTemp.toFixed(1));
+  return convertedTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
