51c66a2618644f7f23c6fa1838650fe6a9465590
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-12 15:23:46 -0500

    refactor after seeing solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,7 @@
 const leapYears = function (year) {
-  if (Number.isInteger(year / 4) && !Number.isInteger(year / 100)) {
-    return true;
-  } else if (
+  if (
     Number.isInteger(year / 4) &&
-    Number.isInteger(year / 100) &&
-    Number.isInteger(year / 400)
+    (!Number.isInteger(year / 100) || Number.isInteger(year / 400))
   ) {
     return true;
   } else {
