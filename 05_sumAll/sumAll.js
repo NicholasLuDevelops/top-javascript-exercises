0eec0ed50372a940972056025105112d9f99ed29
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-12 14:43:05 -0500

    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  if (
+    first < 0 ||
+    last < 0 ||
+    !Number.isInteger(first) ||
+    !Number.isInteger(last)
+  )
+    return "ERROR";
 
+  let sum = 0;
+
+  switch (first < last) {
+    case true:
+      while (first <= last) {
+        sum += first;
+        first++;
+      }
+      break;
+
+    case false:
+      while (first >= last) {
+        sum += first;
+        first--;
+      }
+
+    default:
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
