56a8ed8ac8da8ca3483f295e56e73f2f66bb52c0
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-12 14:23:49 -0500

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function(array, removeValue) {
-    
+const removeFromArray = function (array, ...removeValue) {
+  let tempArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (removeValue.includes(array[i])) continue;
+    tempArr.push(array[i]);
+  }
+
+  return tempArr;
 };
 
 // Do not edit below this line
